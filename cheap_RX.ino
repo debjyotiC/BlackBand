5e2e6835aababbd068c94dbd4aee6a76c3e31d13
djbabai <djbabai.debjyoti@gmail.com>
2016-07-25 20:39:27 +0530

    Add files via upload

---
+++ cheap_RX.ino
@@ -0,0 +1,45 @@
+# include <SoftwareSerial.h>
+# include <IRCom.h>
+
+# define rxPin 2
+# define txPin 3
+
+# define USERNAME "Hello from band: 1"
+
+SoftwareSerial ir(rxPin,txPin);
+
+int count=0;
+void setup(){
+  setIRpin(9);
+ 
+  ir.begin(2400);
+  Serial.begin(115200);
+  pinMode(13, OUTPUT);
+  
+}
+
+void loop(){
+    if(ir.available()>0) {
+    receiverIR();
+    digitalWrite(13,1);
+   }   
+   else {
+    //delay(10);   
+    digitalWrite(13,0);
+    transmitIR();
+   }
+   delay(10);
+}
+
+void receiverIR(){
+     char val = ir.read();
+     if(val>0){
+     Serial.print(val);
+     
+    }
+}
+void transmitIR(){
+   ir.println(USERNAME);
+ }
+
+
